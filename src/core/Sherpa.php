e1cefe1a5f39c3ffd0bc61966ede4bcac23608c2
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-02 23:18:07 -0500

    feat: add conventions constants to Sherpa main class

--- src/core/Sherpa.php
+++ src/core/Sherpa.php
@@ -4,5 +4,18 @@ namespace Sherpa\Core\core;
 
 class Sherpa
 {
+    /*
+     * Framework Information
+     */
+
     public const string VERSION = "0.1-dev";
+
+
+    /*
+     * Framework Conventions
+     */
+
+    public const string SHERPA_DATA_PREFIX = "sherpaf__";
+
+    public const string DEFAULT_CONTROLLER_METHOD = "__default";
 }
