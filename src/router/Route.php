45885395360ecd1108ce66eecda1bac84245a1cf
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-02 23:19:12 -0500

    refactor: convert asymmetric attributes to g/s methods

--- src/router/Route.php
+++ src/router/Route.php
@@ -2,15 +2,16 @@
 
 namespace Sherpa\Core\router;
 
+use Sherpa\Core\core\Sherpa;
 use Sherpa\Core\router\http\HttpMethod;
 
 class Route
 {
-    public private(set) HttpMethod $httpMethod;
-    public private(set) string $path;
-    public private(set) string $controllerClass;
-    public private(set) string $controllerMethod;
-    public private(set) ?string $name;
+    private HttpMethod $httpMethod;
+    private string $path;
+    private string $controllerClass;
+    private string $controllerMethod;
+    private ?string $name;
 
     public function __construct(
         HttpMethod $httpMethod,
@@ -26,38 +27,77 @@ class Route
         $this->name = $name;
     }
 
-    public function httpMethod(HttpMethod $httpMethod): self
+    public function httpMethod(?HttpMethod $httpMethod = null): self|HttpMethod
     {
-        $this->httpMethod = $httpMethod;
+        if ($httpMethod !== null)
+        {
+            $this->httpMethod = $httpMethod;
+
+            return $this;
+        }
+
+        return $this->httpMethod;
+    }
+
+    public function path(?string $path = null): self|string
+    {
+        if ($path !== null)
+        {
+            $this->path = $path;
+
+            return $this;
+        }
+
+        return $this->path;
+    }
+
+    public function controller(?string $controllerClass = null): self|string
+    {
+        if ($controllerClass !== null)
+        {
+            $this->controllerClass = $controllerClass;
+
+            return $this;
+        }
+
+        return $this->controllerClass;
+    }
+
+    public function method(?string $controllerMethod = null): self|string
+    {
+        if ($controllerMethod !== null)
+        {
+            $this->controllerMethod = $controllerMethod;
+
+            return $this;
+        }
+
+        return $this->controllerMethod;
+    }
+
+    public function removeMethod(): self
+    {
+        $this->controllerMethod = Sherpa::DEFAULT_CONTROLLER_METHOD;
 
         return $this;
     }
 
-    public function path(string $path): self
+    public function name(?string $name = null): self|string
     {
-        $this->path = $path;
+        if ($name !== null)
+        {
+            $this->name = $name;
 
-        return $this;
+            return $this;
+        }
+
+        return $this->name;
     }
 
-    public function controller(string $controllerClass): self
+    public function removeName(): self
     {
-        $this->controllerClass = $controllerClass;
+        $this->name = null;
 
         return $this;
-    }
-
-    public function method(?string $controllerMethod = null): self
-    {
-        $this->controllerMethod = $controllerMethod;
-
-        return $this;
-    }
-
-    public function name(string $name): self
-    {
-        $this->name = $name;
-
-        return $thid;
     }
 }
