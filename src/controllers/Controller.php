e0fe4fb399f707c4df2f3e08562ccbb1aacaf217
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-03 02:19:27 -0500

    fix: add $request to __default() abstract

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -2,7 +2,9 @@
 
 namespace Sherpa\Core\controllers;
 
+use Sherpa\Core\router\Request;
+
 abstract class Controller
 {
-    abstract public function __default();
+    abstract public function __default(Request $request);
 }
