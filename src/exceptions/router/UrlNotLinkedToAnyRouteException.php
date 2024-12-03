8b543ce7de8fccac1bf84dad668c2cb249e03422
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-02 23:29:29 -0500

    feat: create UrlNotLinkedToAnyRouteException class

---
+++ src/exceptions/router/UrlNotLinkedToAnyRouteException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sherpa\Core\exceptions\router;
+
+use Sherpa\Core\exceptions\SherpaException;
+use Throwable;
+
+class UrlNotLinkedToAnyRouteException extends SherpaException
+{
+    public function __construct(string $url,
+                                ?Throwable $previous = null)
+    {
+        $code = "SHERPA_EXC_UNLTAR_IR01";
+        $message = "Following URL is no longer linked to any route: "
+                 . "$url";
+
+        parent::__construct($message, $code, $previous);
+    }
+}
