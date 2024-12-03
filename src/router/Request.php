57a76f671b7b1690dfe11cb974113c0fa8128e0c
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-02 23:20:31 -0500

    feat: create Request class

---
+++ src/router/Request.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sherpa\Core\router;
+
+use Sherpa\Core\router\http\HttpMethod;
+use Sherpa\Core\router\utils\URI;
+
+class Request
+{
+    public private(set) HttpMethod $httpMethod;
+
+    public private(set) string $url;
+
+    private array $data;
+    public private(set) array $sherpaData;
+
+    public function __construct()
+    {
+        $this->httpMethod = HttpMethod::from($_SERVER["REQUEST_METHOD"]);
+        $this->url = $_GET["sherpaf__path"] ?? "";
+        $this->data = URI::getExternalData();
+        $this->sherpaData = URI::getSherpaData();
+    }
+
+    public function data(?string $key = null): mixed
+    {
+        return $key !== null
+            ? $this->data[$key]
+            : $this->data;
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($this->data[$key]);
+    }
+}
