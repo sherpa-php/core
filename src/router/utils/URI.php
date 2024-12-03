729de9b5c47e910116bc71738b6b1f2d99b693b0
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-03 00:01:43 -0500

    fix: remove cookies from request data array

--- src/router/utils/URI.php
+++ src/router/utils/URI.php
@@ -8,12 +8,13 @@ class URI
 {
     public static function getExternalData(): array
     {
-        return array_filter($_REQUEST, function ($dataKey)
-        {
-            return !str_starts_with(
-                $dataKey,
-                Sherpa::SHERPA_DATA_PREFIX);
-        }, ARRAY_FILTER_USE_KEY);
+        return array_filter(array_merge($_GET, $_POST),
+            function ($dataKey)
+            {
+                return !str_starts_with(
+                    $dataKey,
+                    Sherpa::SHERPA_DATA_PREFIX);
+            }, ARRAY_FILTER_USE_KEY);
     }
 
     public static function getSherpaData(): array
@@ -24,5 +25,22 @@ class URI
                 $dataKey,
                 Sherpa::SHERPA_DATA_PREFIX);
         }, ARRAY_FILTER_USE_KEY);
+    }
+
+    private static function getData(): array
+    {
+        return array_merge($_GET, $_POST, self::getPutAndDeleteData());
+    }
+
+    private static function getPutAndDeleteData(): array
+    {
+        if ($_SERVER["REQUEST_METHOD"] === "PUT"
+            || $_SERVER["REQUEST_METHOD"] === "DELETE")
+        {
+            parse_str(file_get_contents("php://input"), $data);
+            return $data;
+        }
+
+        return [];
     }
 }
