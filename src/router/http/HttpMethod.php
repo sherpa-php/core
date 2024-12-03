19dc1cecb2f07bf07f0a2c424ad4114c41e8bfcf
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-02 23:18:38 -0500

    feat: add from() method to HttpMethod enum

--- src/router/http/HttpMethod.php
+++ src/router/http/HttpMethod.php
@@ -9,4 +9,18 @@ enum HttpMethod
     case HEAD;
     case PUT;
     case DELETE;
+
+    public static function from(string $method): self
+    {
+        return match (strtoupper($method))
+        {
+            "GET" => self::GET,
+            "POST" => self::POST,
+            "HEAD" => self::HEAD,
+            "PUT" => self::PUT,
+            "DELETE" => self::DELETE,
+            default => throw new \InvalidArgumentException(
+                "Invalid HTTP method: $method"),
+        };
+    }
 }
