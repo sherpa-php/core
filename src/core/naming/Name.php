1cf1466d83f3c329a05d63726f4e727876e72d15
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-04 00:35:18 -0500

    feat: create Name utility class
    #15

---
+++ src/core/naming/Name.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sherpa\Core\core\naming;
+
+use Symfony\Component\String\Inflector\EnglishInflector;
+use function Symfony\Component\String\u;
+
+class Name
+{
+    public static function getDBNameFromModel(string $model): string
+    {
+        $english = new EnglishInflector();
+        $modelName = u(basename($model));
+
+        return $english->pluralize($modelName)[0]
+            ?? $modelName->toString();
+    }
+}
