94415a3b4f75aa7aaf0060d4512e6367e643c9ce
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-04 00:35:09 -0500

    feat: add model's table naming bases

--- src/models/Model.php
+++ src/models/Model.php
@@ -2,8 +2,18 @@
 
 namespace Sherpa\Core\models;
 
+use Sherpa\Core\core\naming\Name;
+
 class Model
 {
+    /** Database Table name. */
+    public string $table;
+
+    public function __construct()
+    {
+        $this->table = Name::getDBNameFromModel(static::class);
+    }
+
     private static array $public = [];
     private static array $hidden = [];
 }
