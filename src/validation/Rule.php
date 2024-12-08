e0f3cf78d9335048f81fa28368c5170cad4abcca
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-08 17:46:36 -0500

    feat: PHPdocs + methods

--- src/validation/Rule.php
+++ src/validation/Rule.php
@@ -2,9 +2,29 @@
 
 namespace Sherpa\Core\validation;
 
+use Sherpa\Core\router\Request;
 use Sherpa\Core\validation\enums\Response;
 
-interface Rule
+/**
+ * Rule main class.
+ * <p>
+ *     Implements all rule utilities methods.
+ * </p>
+ */
+abstract class Rule
 {
-    public function handle(): bool|Response;
+    public private(set) bool $response;
+
+    /**
+     * Used by Validator for verifying to get state.
+     *
+     * @param Request $request
+     * @return bool|Response Rule response state
+     */
+    public abstract function handle(Request $request): bool|Response;
+
+    /**
+     * @return string Rule error message
+     */
+    public abstract function getError(): string;
 }
