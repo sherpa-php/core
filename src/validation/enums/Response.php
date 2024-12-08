11717c2a574aa19c71925aa7c0ad383f94993ac3
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-08 17:49:32 -0500

    feat: add from() method to Response enum

--- src/validation/enums/Response.php
+++ src/validation/enums/Response.php
@@ -6,4 +6,13 @@ enum Response
 {
     case ACCEPT;
     case DENY;
+
+    public static function from(bool $value): self
+    {
+        return match ($value)
+        {
+            true => Response::ACCEPT,
+            default => Response::DENY,
+        };
+    }
 }
