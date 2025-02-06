a0e3f38b29096ab54b1c8866b5fb3f5506a9bbaa
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-06 17:39:40 -0500

    feat: create "is" functions for each image format
    #46

--- src/files/Image.php
+++ src/files/Image.php
@@ -4,5 +4,93 @@ namespace Sherpa\Core\files;
 
 class Image extends File
 {
+    /**
+     * (mirror method)
+     * @return bool Is JPEG format
+     * @see Image::isJpeg()
+     */
+    public function isJpg(): bool
+    {
+        return $this->isJpeg();
+    }
 
+    /**
+     * @return bool Is JPEG format
+     */
+    public function isJpeg(): bool
+    {
+        return $this->mime->format === "jpeg";
+    }
+
+    /**
+     * @return bool Is PNG format
+     */
+    public function isPng(): bool
+    {
+        return $this->mime->format === "png";
+    }
+
+    /**
+     * @return bool Is GIF format
+     */
+    public function isGif(): bool
+    {
+        return $this->mime->format === "gif";
+    }
+
+    /**
+     * @return bool Is BMP format
+     */
+    public function isBmp(): bool
+    {
+        return $this->mime->format === "bmp";
+    }
+
+    /**
+     * @return bool Is WEBP format
+     */
+    public function isWebp(): bool
+    {
+        return $this->mime->format === "webp";
+    }
+
+    /**
+     * @return bool Is TIFF format
+     */
+    public function isTiff(): bool
+    {
+        return $this->mime->format === "tiff";
+    }
+
+    /**
+     * @return bool Is SVG format
+     */
+    public function isSvg(): bool
+    {
+        return $this->mime->format === "svg+xml";
+    }
+
+    /**
+     * @return bool Is X-ICON format
+     */
+    public function isXIcon(): bool
+    {
+        return $this->mime->format === "x-icon";
+    }
+
+    /**
+     * @return bool Is HEIF format
+     */
+    public function isHeif(): bool
+    {
+        return $this->mime->format === "heif";
+    }
+
+    /**
+     * @return bool Is HEIC format
+     */
+    public function isHeic(): bool
+    {
+        return $this->mime->format === "heic";
+    }
 }
