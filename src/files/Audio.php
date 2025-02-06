9df7d990fda9f4d0a8dfdbf18fcd42c3f05e6bd4
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-06 17:39:56 -0500

    feat: create "is" functions for each audio format
    #46

--- src/files/Audio.php
+++ src/files/Audio.php
@@ -4,5 +4,85 @@ namespace Sherpa\Core\files;
 
 class Audio extends File
 {
+    /**
+     * (mirror method)
+     * @return bool Is MPEG/MP3 format
+     * @see Audio::isMpeg()
+     */
+    public function isMp3(): bool
+    {
+        return $this->isMpeg();
+    }
 
+    /**
+     * @return bool Is MPEG format
+     */
+    public function isMpeg(): bool
+    {
+        return $this->mime->format === "mpeg";
+    }
+
+    /**
+     * @return bool Is WAV format
+     */
+    public function isWav(): bool
+    {
+        return $this->mime->format === "mpeg";
+    }
+
+    /**
+     * @return bool Is OGG format
+     */
+    public function isOgg(): bool
+    {
+        return $this->mime->format === "mpeg";
+    }
+
+    /**
+     * @return bool Is AAC format
+     */
+    public function isAac(): bool
+    {
+        return $this->mime->format === "aac";
+    }
+
+    /**
+     * @return bool Is FLAC format
+     */
+    public function isFlac(): bool
+    {
+        return $this->mime->format === "flac";
+    }
+
+    /**
+     * @return bool Is WEBM format
+     */
+    public function isWebm(): bool
+    {
+        return $this->mime->format === "webm";
+    }
+
+    /**
+     * @return bool Is X-MS-WMA format
+     */
+    public function isXmsWma(): bool
+    {
+        return $this->mime->format === "x-ms-wma";
+    }
+
+    /**
+     * @return bool Is X-WAV format
+     */
+    public function isXWav(): bool
+    {
+        return $this->mime->format === "x-wav";
+    }
+
+    /**
+     * @return bool Is X-AIFF format
+     */
+    public function isXAiff(): bool
+    {
+        return $this->mime->format === "x-aiff";
+    }
 }
