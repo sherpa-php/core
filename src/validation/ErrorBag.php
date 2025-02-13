8a4fb83bcfa41132f63b6268b5d5e31fb666e5ea
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-12 19:02:23 -0500

    refactor: extends Bag to ErrorBag
    instead of defining ErrorBag as other main class

--- src/validation/ErrorBag.php
+++ src/validation/ErrorBag.php
@@ -2,75 +2,17 @@
 
 namespace Sherpa\Core\validation;
 
-class ErrorBag
+use Sherpa\Core\containment\Bag;
+
+/**
+ * Validator errors' bag.
+ * <p>
+ *     It contains all the validation's
+ *     errors.
+ * </p>
+ *
+ * @see Bag
+ */
+class ErrorBag extends Bag
 {
-    public private(set) array $errors = [];
-
-    /**
-     * @return bool Is errors bag empty
-     */
-    public function isEmpty(): bool
-    {
-        return !count($this->errors);
-    }
-
-    /**
-     * @return bool Is errors bag not empty
-     */
-    public function isNotEmpty(): bool
-    {
-        return !$this->isEmpty();
-    }
-
-    /**
-     * Adds an error to bag
-     *
-     * @param string $key
-     * @param string $error
-     * @return $this
-     */
-    public function add(string $key, string $error): self
-    {
-        $this->errors[$key] = $error;
-
-        return $this;
-    }
-
-    /**
-     * Removes error from bag by its key.
-     *
-     * @param string $key
-     * @return $this
-     */
-    public function remove(string $key): self
-    {
-        if ($this->has($key))
-        {
-            unset($this->errors[$key]);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param string $key Error's key
-     * @return bool Is error key existing
-     */
-    public function has(string $key): bool
-    {
-        return isset($this->errors[$key]);
-    }
-
-    /**
-     * @param string|null $key (optional) error's key
-     * @return array|string|null Errors array or error value if exists
-     */
-    public function get(?string $key = null): array|string|null
-    {
-        return $key === null
-            ? $this->errors
-            : ($this->has($key)
-                ? $this->errors[$key]
-                : null);
-    }
 }
