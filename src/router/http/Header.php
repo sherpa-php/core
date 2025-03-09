b8df70d6bea29bc20fe87f739e05514064c8d1c8
belicfr <96206878+belicfr@users.noreply.github.com>
2025-03-09 18:53:56 -0400

    feat: create Header bag class

---
+++ src/router/http/Header.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sherpa\Core\router\http;
+
+use Sherpa\Core\containment\Bag;
+
+/**
+ * Request's headers class.
+ * <p>
+ *     For recording all request's headers.
+ * </p>
+ */
+class Header extends Bag
+{
+
+}
