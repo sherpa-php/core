9adeb322c486ca2ee53173c663b9f3618224252a
belicfr <96206878+belicfr@users.noreply.github.com>
2025-03-13 18:44:18 -0400

    feat: implement locales getter and validator into Header bag class

--- src/router/http/Header.php
+++ src/router/http/Header.php
@@ -3,6 +3,7 @@
 namespace Sherpa\Core\router\http;
 
 use Sherpa\Core\containment\Bag;
+use Sherpa\Core\router\exceptions\InvalidHeaderLocalesException;
 
 /**
  * Request's headers class.
@@ -12,5 +13,41 @@ use Sherpa\Core\containment\Bag;
  */
 class Header extends Bag
 {
+    private const string LOCALES_REGEX
+        = "/^[a-z]{2,3}(?:-[A-Z]{2,3})?(?:;q=0\.[0-9])?(?:,[a-z]{2,3}(?:-[A-Z]{2,3})?(?:;q=0\.[0-9])?)*$/";
 
+    /**
+     * @return Bag Request's locales bag
+     * @throws InvalidHeaderLocalesException
+     */
+    public function locales(): Bag
+    {
+        if (!$this->isLocalesValid())
+        {
+            throw new InvalidHeaderLocalesException();
+        }
+
+        $localesHeader = $this->getLocalesAttribute();
+
+        return new Bag(explode(',', $localesHeader))
+            ->sanitize();
+    }
+
+    /**
+     * @return bool Is request's locales' header attribute valid?
+     */
+    public function isLocalesValid(): bool
+    {
+        return preg_match(
+            self::LOCALES_REGEX,
+            $this->getLocalesAttribute());
+    }
+
+    /**
+     * @return string Request's locales' header attribute
+     */
+    private function getLocalesAttribute(): string
+    {
+        return $this->get("Accept-Language") ?? "";
+    }
 }
