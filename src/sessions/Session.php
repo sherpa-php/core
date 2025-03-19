eae31224f8cd16911f91bb4d73d8e83ff60d7f1d
belicfr <96206878+belicfr@users.noreply.github.com>
2025-03-19 19:31:29 -0400

    fix: set Session model's table name to "sherpa_sessions"

--- src/sessions/Session.php
+++ src/sessions/Session.php
@@ -6,5 +6,10 @@ use Sherpa\Core\models\Model;
 
 class Session extends Model
 {
+    public function __construct(array $public, array $private)
+    {
+        parent::__construct($public, $private);
 
+        self::setTable("sherpa_sessions");
+    }
 }
