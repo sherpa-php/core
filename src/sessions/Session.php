023ad3aa16ca59c49414fdfea583052a95345eae
belicfr <96206878+belicfr@users.noreply.github.com>
2025-03-20 18:05:49 -0400

    docs: add missing PHPdoc and add `createOrRetrieve()`

--- src/sessions/Session.php
+++ src/sessions/Session.php
@@ -11,10 +11,54 @@ class Session extends Model
     ];
 
 
+    /**
+     * Retrieve session row from sessions table
+     * using user ID.
+     *
+     * @param int $userId
+     * @return Session|null
+     */
     public static function getByUserId(int $userId): ?Session
     {
         return Session::query()
                       ->where("user_id", $userId)
                       ->first();
+    }
+
+    /**
+     * Attempt to retrieve the current session's row.
+     * If it does no longer exist, it will be created
+     * and returned.
+     *
+     * @return Session
+     */
+    public static function createOrRetrieve(): Session
+    {
+        if (!self::exists())
+        {
+            return Session::query()->create([
+                "token" => SessionsManager::token(),
+                "user_id" => null,  // TODO: implement user_id
+                "data" => "",
+            ]);
+        }
+        else
+        {
+            return Session::query()
+                          ->where("token", SessionsManager::token())
+                          ->first();
+        }
+    }
+
+    /**
+     * @return bool If current session is existing
+     */
+    public static function exists(): bool
+    {
+        $token = SessionsManager::token();
+
+        return Session::query()
+                      ->where("token", $token)
+                      ->count();
     }
 }
